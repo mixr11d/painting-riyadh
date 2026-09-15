f256e3dd3f9f7b4f5d354b2b9bcd84b9ecebc491
mixr11d <mix.r11d@gmail.com>
2026-09-15 09:32:06 +0300

    Refactor service worker install and activate events

--- sw.js
+++ sw.js
@@ -1,22 +1,7 @@
-// تحديث وإلغاء الكاش لضمان وصول التحديثات والإعلانات فوراً
-const CACHE_NAME = 'paint-riyadh-v2.0.0';
-
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.map((key) => caches.delete(key)));
-    })
+    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
   );
   self.clients.claim();
-});
-
-// الاعتماد على جلب البيانات الحية من السيرفر مباشرة
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
 });
